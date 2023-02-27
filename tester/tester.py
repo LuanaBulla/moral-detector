fa680fdc94f63b2ff6832a4ec2a63cc46e215dc8
LuanaBulla <77782240+LuanaBulla@users.noreply.github.com>
2023-02-27 07:30:30 +0100

    Update tester.py

--- tester/tester.py
+++ tester/tester.py
@@ -3,8 +3,8 @@ from moral_detector import MoralDetector
 def main():
   detector = MoralDetector()
   txt = input(str)
-  return detector.execution(txt)
+  print(detector.execution(txt))
   
 if __name__ == "__main__":
-    print(main())
+    main()
 
