7eb8a89a6ba71193726e9dab95aa69a1325c72c1
LuanaBulla <77782240+LuanaBulla@users.noreply.github.com>
2023-02-27 10:46:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ from setuptools import setup
 setup(
     name='tester',
     version='1.0',
-    packages=['tester'])
+    packages=['tester'],
+    license='CC BY-NC-SA 4.0',
+    author='Luana Bulla',
+    author_email='{luana.bulla@istc.cnr.it')
